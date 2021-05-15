f3fb73af87657ef27e7b2ff66d4696f5345d6759
putrijawak <82867153+putrijawak@users.noreply.github.com>
2021-05-15 11:20:40 +0700

    Add files via upload

---
+++ hapus_data.php
@@ -0,0 +1,19 @@
+<?php
+include 'koneksi.php';
+
+if (isset($_GET['aa'])) {
+	$id = stripslashes(strip_tags(htmlspecialchars( base64_decode($_GET['aa']) ,ENT_QUOTES)));
+
+	$query = "DELETE FROM form WHERE id=?";
+    $dewan1 = $db1->prepare($query);
+    $dewan1->bind_param("i", $id);
+    
+    if ($dewan1->execute()) {
+    	echo "<script>alert('Data Berhasil Dihapus');location='data.php';</script>";
+    } else {
+    	echo "<script>alert('Error');window.history.go(-1);</script>";
+    }
+}
+
+$db1->close();
+?>
