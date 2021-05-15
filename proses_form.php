43dade2bf828df74939dd0b7611b3636c09847ee
silviasrl <82866884+silviasrl@users.noreply.github.com>
2021-05-15 19:57:21 +0700

    Add files via upload

--- proses_form.php
+++ proses_form.php
@@ -2,14 +2,17 @@
   include "koneksi_form.php";
   $nama  = $_REQUEST['nama'];
   $telepon  = $_REQUEST['telepon'];
+  $tiket = $_REQUEST['tiket'];
+  $stasiun = $_REQUEST['stasiun'];
   $hari  = $_REQUEST['hari'];
   $jam  = $_REQUEST['jam'];
   $metode_pembayaran  = $_REQUEST['metode_pembayaran'];
-  $mysqli  = "INSERT INTO form (nama_lengkap, no_telepon, hari_berangkat, jam_berangkat, metode_pembayaran) VALUES ('$nama', '$telepon', '$hari', '$jam', '$metode_pembayaran')";
+  $mysqli  = "INSERT INTO form (nama_lengkap, no_telepon, tiket, stasiun, hari_berangkat, jam_berangkat, metode_pembayaran) VALUES ('$nama', '$telepon','$tiket','$stasiun', '$hari', '$jam', '$metode_pembayaran')";
   $result  = mysqli_query($conn, $mysqli);
 
   if ($result) {
     echo "Input berhasil";
+    header("location:hasiltiket.php");
   } else {
     echo "Input gagal";
   }
