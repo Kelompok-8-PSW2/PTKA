0b3486d2e2d83c2ea198f177993de69888429fd2
putrijawak <82867153+putrijawak@users.noreply.github.com>
2021-05-15 19:32:34 +0700

    Add files via upload

--- layanan.php
+++ layanan.php
@@ -10,6 +10,7 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="stylee.css">
 </head>
 <body>
     <div class="col-md-4" style="background-color:cyan ;">
@@ -44,13 +45,13 @@
     <nav class="navbar navbar-expand-sm bg navbar-dark">
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
-                <a href="#tampilan1" class="nav-link">Beranda</a>
+                <a href="beranda.php" class="nav-link">Beranda</a>
             </li>
             <li class="nav-item">
-                <a href="#tampilan2" class="nav-link">Layanan</a>
+                <a href="layanan.php" class="nav-link">Layanan</a>
             </li>
             <li class="nav-item">
-                <a href="#tampilan3" class="nav-link">Login</a>
+                <a href="login.php" class="nav-link">Login</a>
             </li>
         </ul>
     </nav>
@@ -127,9 +128,10 @@
             </div>
         </div>
        </div>
-
-                    <br><br><br><br><br><br><br><br><br><br>
-                    <p style="text-align: right;background-color: grey;">AMAN,NYAMAN,TERPERCAYA</p>
+        <br><br><br><b><br></b><br><br><br>
     </div>
+    <div class="col-md-8">
+    <p class= "tulisan_satu">AMAN,NYAMAN,TERPERCAYA</p><br>
+    <center><div class="logout"><a href="logout.php" class="form-inline my-2 my-lg-0 btn btn-secondary">Keluar</a></div></center>
 </body>
 </html>
